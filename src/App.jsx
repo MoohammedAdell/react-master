4dc8cebdd2ee88396c943b0b843020ca0de4c5bc
MoohammedAdell <mohammedadell496@gmail.com>
2026-07-27 18:50:43 +0300

    Refactor App component to fetch users from API and remove CRUD functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,25 @@
-import { useState } from "react";
-import UserCard from "./components/UserCard";
-
-const initialUsers = [
-  { id: 1, name: "Mohamed", age: 22 },
-  { id: 2, name: "Ahmed", age: 20 },
-  { id: 3, name: "Ali", age: 25 },
-];
+import { useEffect, useState } from "react";
+import Crud from "./components/crudOperation/Crud";
 
 function App() {
-  const [users, setUsers] = useState(initialUsers);
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [edit, setEdit] = useState(null);
+  const [users, setUsers] = useState([]);
 
-  const handelAddUser = () => {
-    const newUser = {
-      id: Date.now,
-      name: name,
-      age: Number(age),
-    };
-    if (name.trim().length > 0 && age > 0) {
-      setUsers((prev) => [...prev, newUser]);
+  useEffect(() => {
+    async function getUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
     }
-    setName("");
-    setAge("");
-  };
+    getUsers();
+  }, []);
 
-  const handleDelete = (id) => {
-    setUsers((prev) => prev.filter((user) => user.id !== id));
-  };
-
-  const handelEdit = (user) => {
-    setEdit(user);
-    setName(user.name);
-    setAge(user.age);
-  };
-  const handleUpdateUser = (id) => {
-    setUsers((prev) =>
-      prev.map((user) => {
-        if (user.id === edit.id) {
-          return {
-            ...user,
-            name,
-            age: Number(age),
-          };
-        }
-
-        return user;
-      }),
-    );
-    setEdit(null);
-    setName("");
-    setAge("");
-  };
   return (
     <>
-      <input
-        type="text"
-        placeholder="Enter Your Name.."
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Enter Your Age.."
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <button onClick={edit ? handleUpdateUser : handelAddUser}>
-        {edit ? "Update User" : "Add User"}
-      </button>
-
       {users.map((user) => (
-        <UserCard
-          key={user.id}
-          user={user}
-          handleDelete={handleDelete}
-          handelEdit={handelEdit}
-          handleUpdateUser={handleUpdateUser}
-        />
+        <h1 key={user.id}>{user.name}</h1>
       ))}
     </>
   );
---
+++ src/components/crudOperation/Crud.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import UserCard from "../UserCard";
+
+const initialUsers = [
+  { id: 1, name: "Mohamed", age: 22 },
+  { id: 2, name: "Ahmed", age: 20 },
+  { id: 3, name: "Ali", age: 25 },
+];
+
+export default function Crud() {
+  const [users, setUsers] = useState(initialUsers);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [edit, setEdit] = useState(null);
+
+  const handelAddUser = () => {
+    const newUser = {
+      id: Date.now,
+      name: name,
+      age: Number(age),
+    };
+    if (name.trim().length > 0 && age > 0) {
+      setUsers((prev) => [...prev, newUser]);
+    }
+    setName("");
+    setAge("");
+  };
+
+  const handleDelete = (id) => {
+    setUsers((prev) => prev.filter((user) => user.id !== id));
+  };
+
+  const handelEdit = (user) => {
+    setEdit(user);
+    setName(user.name);
+    setAge(user.age);
+  };
+  const handleUpdateUser = (id) => {
+    setUsers((prev) =>
+      prev.map((user) => {
+        if (user.id === edit.id) {
+          return {
+            ...user,
+            name,
+            age: Number(age),
+          };
+        }
+
+        return user;
+      }),
+    );
+    setEdit(null);
+    setName("");
+    setAge("");
+  };
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter Your Name.."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Enter Your Age.."
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button onClick={edit ? handleUpdateUser : handelAddUser}>
+        {edit ? "Update User" : "Add User"}
+      </button>
+
+      {users.map((user) => (
+        <UserCard
+          key={user.id}
+          user={user}
+          handleDelete={handleDelete}
+          handelEdit={handelEdit}
+          handleUpdateUser={handleUpdateUser}
+        />
+      ))}
+    </>
+  );
+}
