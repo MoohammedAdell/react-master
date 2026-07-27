c6b0a60d87b5cb1ba7de2e06328d6ba6b4baaf8f
MoohammedAdell <mohammedadell496@gmail.com>
2026-07-28 01:16:33 +0300

    Implement user addition functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Crud from "./components/crudOperation/Crud";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
   useEffect(() => {
     async function getUsers() {
@@ -10,14 +12,51 @@ function App() {
         "https://jsonplaceholder.typicode.com/users",
       );
       const data = await response.json();
-      console.log(data);
       setUsers(data);
     }
     getUsers();
   }, []);
 
+  const handelAddUser = () => {
+    async function addUser() {
+      if (name.trim().length === 0 || age <= 0) return;
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: name,
+            age: age,
+          }),
+        },
+      );
+      const data = await response.json();
+      setUsers((prev) => [...prev, data]);
+
+      setName("");
+      setAge("");
+    }
+    addUser();
+  };
+
   return (
     <>
+      <input
+        type="text"
+        placeholder="Enter Name of user.."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Enter Age of user.."
+        value={age}
+        onChange={(e) => setAge(Number(e.target.value))}
+      />
+      <button onClick={handelAddUser}>Add User</button>
       {users.map((user) => (
         <h1 key={user.id}>{user.name}</h1>
       ))}
